e771f6d8b41cc05ad8f277ee6a892019adb33276
h3nc4 <me@h3nc4.com>
2024-03-03 21:36:59 -0300

    dev: github compiling

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,4 @@
 javac -d bin src/**/*
-echo "Main-Class: client.Client" > MANIFEST.MF
-jar cfm client.jar MANIFEST.MF -C bin/ . README.md LICENSE
-echo "Main-Class: server.Server" > MANIFEST.MF
-jar cfm server.jar MANIFEST.MF -C bin/ . README.md LICENSE
-rm -r MANIFEST.MF bin
+jar cfe client.jar client.Client -C bin/ . README.md LICENSE
+jar cfe server.jar server.Server -C bin/ . README.md LICENSE
+rm -r bin
